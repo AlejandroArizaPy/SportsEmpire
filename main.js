048d0c09094f74865181a346a34a3e2aac37d730
AlejandroArizaPy <tunuevoemail@example.com>
2024-02-12 02:03:19 -0500

    Agrega archivos HTML y CSS para el formulario de registro

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
 
+
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
